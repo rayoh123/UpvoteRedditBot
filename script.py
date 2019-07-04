8e9d259f1dbd5971e14fb0512723c4598ad67cf7
Raymond Ouyang <ouyangmingraymond@gmail.com>
2019-07-04 16:32:29 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,28 +1,31 @@
 import praw
 import details
 
-###  LOGGING INTO REDDIT  ###
-reddit_instance = praw.Reddit(user_agent=        'raymond_ouyang_1',
-                              client_id=         details.client_id,
-                              client_secret=     details.secret,
-                              password=          details.password,
-                              username=          details.username)
+def main():
+    ###  LOGGING INTO REDDIT  ###
+    reddit_instance = praw.Reddit(user_agent=        'raymond_ouyang_1',
+                                  client_id=         details.client_id,
+                                  client_secret=     details.secret,
+                                  password=          details.password,
+                                  username=          details.username)
 
 
-### OPENING/CREATING COMMENTED FILE AND RETRIEVING ID'S ###
-f = open('commented.txt', 'r')
-commented = list(filter(None, f.read().split('\n')))
-f.close()
+    ### OPENING/CREATING COMMENTED FILE AND RETRIEVING ID'S ###
+    f = open('commented.txt', 'r')
+    commented = list(filter(None, f.read().split('\n')))
+    f.close()
 
 
 
-### COMMENTING ###
-while True:
-    for comment in reddit_instance.subreddit('freekarma4u').comments(limit=200):
-        if (comment.id not in commented) and (comment.author != reddit_instance.user.me()):            
-            comment.reply("Please upvote me!")
-            print('commented')
-            commented.append(comment.id)
-            with open('commented.txt', 'a') as f:
-                f.write(comment.id + '\n')
+    ### COMMENTING ###
+    while True:
+        for comment in reddit_instance.subreddit('freekarma4u').comments(limit=200):
+            if (comment.id not in commented) and (comment.author != reddit_instance.user.me()):            
+                comment.reply("Please upvote me!")
+                print('commented')
+                commented.append(comment.id)
+                with open('commented.txt', 'a') as f:
+                    f.write(comment.id + '\n')
 
+if __name__ == '__main__':
+    main()
