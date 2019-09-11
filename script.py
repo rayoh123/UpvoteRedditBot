d918ca2fe567f49cbc9620bc300d2805dbdca851
Raymond Ouyang <ouyangmingraymond@gmail.com>
2019-09-10 17:30:04 -0700

    Update script.py

--- script.py
+++ script.py
@@ -10,6 +10,7 @@ reddit_instance = praw.Reddit(user_agent=        'add_your_user_agent_here',
 
 
 ### OPENING/CREATING COMMENTED FILE AND RETRIEVING ID'S ###
+# the 'commented' variable holds a live version of this sessions' replied-to-comments
 f = open('commented.txt', 'r')
 commented = list(filter(None, f.read().split('\n')))
 f.close()
@@ -22,6 +23,8 @@ while True:
         if (comment.id not in commented) and (comment.author != reddit_instance.user.me()):            
             comment.reply("Please upvote me!")
             print('commented')
+            
+            # Adds the replied-to-comment's id into the variable 'commented' and also writes this comment's id into the file
             commented.append(comment.id)
             with open('commented.txt', 'a') as f:
                 f.write(comment.id + '\n')
